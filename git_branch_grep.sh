5530c120c53568a67b4c3a8672219629a5384e49
Marcin Nowicki <pr0d1r2@gmail.com>
2017-06-13 17:39:26 +0200

    Progress with the implementation

--- git_branch_grep.sh
+++ git_branch_grep.sh
@@ -1,7 +1,10 @@
 function git_branch_grep() {
   todo WIP
-  git branch -r | cut -b3- | \
+  git fetch &>/dev/null || git fetch || return $?
+  local git_branch_grep_COMMAND='SHA=`git rev-parse {}` && git diff $SHA $(git merge-base origin/master $SHA) | grep "^+"'
+  git_branch_grep_COMMAND+=" | grep "$@" ; exit 0"
+  git branch -r | cut -b10- | \
     parallel \
       --halt-on-error now,fail=1 \
-      "SHA=\`git rev-parse {}\` && git diff \$SHA \$(git merge-base origin/master \$SHA) | grep '^+'"
+      $git_branch_grep_COMMAND
 }
