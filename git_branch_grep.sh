a66a90db38271dd1260ec7aac55fc39452eb3365
Marcin Nowicki <pr0d1r2@gmail.com>
2017-06-13 19:51:57 +0200

    Allow to grep all remote branches for code

--- git_branch_grep.sh
+++ git_branch_grep.sh
@@ -1,9 +1,24 @@
 function git_branch_grep() {
-  todo WIP
   git fetch &>/dev/null || git fetch || return $?
-  local git_branch_grep_COMMAND='SHA=`git rev-parse {}` && git diff $SHA $(git merge-base origin/master $SHA) | grep "^+"'
-  git_branch_grep_COMMAND+=" | grep "$@" ; exit 0"
-  git branch -r | cut -b10- | \
+  local git_branch_grep_BASE_COMMAND
+  git_branch_grep_BASE_COMMAND='SHA=`git rev-parse {}`'
+  git_branch_grep_BASE_COMMAND+=' && git diff $SHA $(git merge-base origin/master $SHA)'
+  git_branch_grep_BASE_COMMAND+=' | grep "^+"'
+  local git_branch_grep_SEPARATOR_COMMAND
+  git_branch_grep_SEPARATOR_COMMAND='echo "=========================================="'
+  local git_branch_grep_COMMAND
+  git_branch_grep_COMMAND="$git_branch_grep_BASE_COMMAND | grep -q '$@';"
+  git_branch_grep_COMMAND+='if [ $? -eq 0 ]; then'
+  git_branch_grep_COMMAND+="  $git_branch_grep_SEPARATOR_COMMAND;"
+  git_branch_grep_COMMAND+='  echo "{}";'
+  git_branch_grep_COMMAND+="  $git_branch_grep_SEPARATOR_COMMAND;"
+  git_branch_grep_COMMAND+="  $git_branch_grep_BASE_COMMAND | grep '$@'"
+  git_branch_grep_COMMAND+='  | cut -b1-256;'
+  git_branch_grep_COMMAND+='  exit 0;'
+  git_branch_grep_COMMAND+='else'
+  git_branch_grep_COMMAND+='  exit 0;'
+  git_branch_grep_COMMAND+='fi'
+  git branch -r | cut -b3- | grep -v '^origin/HEAD -> origin/master$' | \
     parallel \
       --halt-on-error now,fail=1 \
       $git_branch_grep_COMMAND
