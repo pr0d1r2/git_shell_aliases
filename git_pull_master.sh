5e99c74b916513033d61d287115796381877bfc1
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-24 17:52:51 +0100

    Update master whatever state of tree is

---
+++ git_pull_master.sh
@@ -0,0 +1,29 @@
+function git_pull_master() {
+  local git_pull_master_NOT_CLEAN
+  if ! git_clean; then
+    git_pull_master_NOT_CLEAN=1
+    overcommit_disable git stash || return $?
+  else
+    git_pull_master_NOT_CLEAN=0
+  fi
+  local git_pull_master_BRANCH
+  git_pull_master_BRANCH="$(git_current_branch)"
+  case "$git_pull_master_BRANCH" in
+    "master");;
+    *)
+      overcommit_disable git checkout master || return $?
+      ;;
+  esac
+  overcommit_disable git pull || return $?
+  # shellcheck disable=SC2048
+  $* || return $?
+  case "$git_pull_master_BRANCH" in
+    "" | "master");;
+    *)
+      overcommit_disable git checkout "$git_pull_master_BRANCH" || return $?
+      ;;
+  esac
+  if [ $git_pull_master_NOT_CLEAN -eq 1 ]; then
+    overcommit_disable git stash pop || return $?
+  fi
+}
