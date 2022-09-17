d59e564aa3e4ca88a364d7984f53f3eb1ac1a53d
Marcin Nowicki <pr0d1r2@gmail.com>
2022-09-17 10:43:06 +0200

    Also consider renamed files

--- git_files_changed_in_branch.sh
+++ git_files_changed_in_branch.sh
@@ -1,3 +1,3 @@
 function git_files_changed_in_branch() {
-  git diff --name-only $(git merge-base origin/master HEAD) --diff-filter=AM
+  git diff --name-only $(git merge-base origin/master HEAD) --diff-filter=AMR
 }
