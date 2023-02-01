df3e8a0bb730cdef807c273fdd60b061ee5f048c
Marcin Nowicki <pr0d1r2@gmail.com>
2023-02-01 08:36:46 +0100

    Use more visible marker

--- git_default_branch.sh
+++ git_default_branch.sh
@@ -1,6 +1,6 @@
 function git_default_branch() {
-  if [ -d .git ]; then
-    echo "Not in git directory!"
+  if [ ! -d .git ]; then
+    echo "NOT_IN_A_GIT_DIRECTORY"
     return 1
   fi
   if [ -f .git/refs/heads/main ]; then
